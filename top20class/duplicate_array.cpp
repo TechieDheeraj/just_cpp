af5ea04cc151dfd2140608bb47c211887def3097
Dheeraj Kakkar <dheeraj.embdeveloper@gmail.com>
2020-02-06 10:01:01 +0530

    Added TS=4, SW=4 and indented the whole code

--- top20class/duplicate_array.cpp
+++ top20class/duplicate_array.cpp
@@ -3,68 +3,68 @@
 #include<limits.h>
 
 /* Problem Statement:
-     Array of n terms ( Elements from 1 to n-1 ), return any duplicate no. present in array.
-*/
+   Array of n terms ( Elements from 1 to n-1 ), return any duplicate no. present in array.
+ */
 using namespace std;
 
 class FindDuplicate {
 
-public:
-    int size;
+	public:
+		int size;
 
-    int findDuplicate1(int *arr, int size) {
-      int i,j;
-      
-      for(i = 0; i < size-1; i++) { // Picked First Element and compared with all rest 
-        for(j=i+1; j< size; j++) { // From Next to 0th element till end of array
-          if( arr[i] == arr[j] )
-             return arr[i];
-        }
-      }
-      return INT_MIN;
-    }
+		int findDuplicate1(int *arr, int size) {
+			int i,j;
 
-    void testcase1(int *arr, int size) {
-      int i;
+			for(i = 0; i < size-1; i++) { // Picked First Element and compared with all rest 
+				for(j=i+1; j< size; j++) { // From Next to 0th element till end of array
+					if( arr[i] == arr[j] )
+						return arr[i];
+				}
+			}
+			return INT_MIN;
+		}
 
-      for(i=0; i < size-1; i++) {
-        arr[i] = i+1;
-      }
-      arr[i] = i;
-    }
+		void testcase1(int *arr, int size) {
+			int i;
 
-    void display(int *arr, int size) {
+			for(i=0; i < size-1; i++) {
+				arr[i] = i+1;
+			}
+			arr[i] = i;
+		}
 
-      int i;
-      cout << "arr[";
-      for(i =0; i < size-1; i++)
-        cout << arr[i] << ", ";
-      cout << arr[i];
+		void display(int *arr, int size) {
 
-      cout << "]" << endl;
-    }
+			int i;
+			cout << "arr[";
+			for(i =0; i < size-1; i++)
+				cout << arr[i] << ", ";
+			cout << arr[i];
+
+			cout << "]" << endl;
+		}
 }; 
 
 int main() {
 
-  FindDuplicate obj;
-  clock_t time;
+	FindDuplicate obj;
+	clock_t time;
 
-  cout << "Enter Size of Array" << endl;
-  cin >> obj.size;
+	cout << "Enter Size of Array" << endl;
+	cin >> obj.size;
 
-  int arr[obj.size];
-    
-  obj.testcase1(arr, obj.size);
+	int arr[obj.size];
 
-  obj.display(arr, obj.size);
- 
-  time = clock();
+	obj.testcase1(arr, obj.size);
 
-  cout << "Duplicate Element is " << obj.findDuplicate1(arr, obj.size) << endl;
-  time = clock() - time;
+	obj.display(arr, obj.size);
 
-  cout << "Time in Secs:" << ((double)time) / (double)CLOCKS_PER_SEC << endl;
- 
-  return 0;
+	time = clock();
+
+	cout << "Duplicate Element is " << obj.findDuplicate1(arr, obj.size) << endl;
+	time = clock() - time;
+
+	cout << "Time in Secs:" << ((double)time) / (double)CLOCKS_PER_SEC << endl;
+
+	return 0;
 }
