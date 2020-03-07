21b81eefa4ff28469944ef466848ca35dc5e4f4c
Dheeraj <Dheeraj.embdeveloper@gmail.com>
2020-03-07 13:34:13 +0000

    Added One More solution to Left Most Leaf Node in Bottom of Tree

--- top20class/binaryTreeTwo.cpp
+++ top20class/binaryTreeTwo.cpp
@@ -36,6 +36,48 @@ class LeftMostNode {
             }
             return -1;
         }
+        
+        // It is being Used just to Pick First Element when NULL Occurs ( To Pick First node from Bottom Level in Tree
+        enum state { NULLSTATE, NON_NULLSTATE};
+
+        int leftMostNode2(BTree *root) {
+            int result= 0;
+            BTree *tmp = NULL;
+            vector<BTree *> queue;
+
+            tmp = root;
+            queue.push_back(tmp);
+            queue.push_back(NULL);
+
+            enum state laststate = NULLSTATE;
+
+            while(!queue.empty()) {
+                tmp = queue.front();
+                queue.erase(queue.begin());
+
+                if(tmp == NULL) {
+                    laststate = NULLSTATE;
+                    if(!queue.empty()) {
+                        queue.push_back(NULL);
+                        continue;
+                    }
+                    else
+                        break;
+                }
+                else {
+                    if(laststate == NULLSTATE) {
+                        result = tmp->data;
+                        laststate = NON_NULLSTATE;
+                    }
+                }
+
+                if(tmp->left != NULL)
+                    queue.push_back(tmp->left);
+                if(tmp->right != NULL)
+                    queue.push_back(tmp->right);
+            }
+            return result;
+        }
 };
         
 int main() {
@@ -47,7 +89,8 @@ int main() {
 
     createOneChildTree();
 
-    cout << "Left Mode Node is " << obj->leftMostNode1(bTreeUtil::root) << endl;
+    //cout << "Left Most Node is " << obj->leftMostNode1(bTreeUtil::root) << endl;
+    cout << "Left Most Node is " << obj->leftMostNode2(bTreeUtil::root) << endl;
 
     return 0;
 }
