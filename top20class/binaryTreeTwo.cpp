7dd71e4bf22acd68037792439d5764c5dc31dc7a
Dheeraj <Dheeraj.embdeveloper@gmail.com>
2020-03-07 15:17:58 +0000

    Implemented Third Solution of Left Most Bottom Node in Tree

--- top20class/binaryTreeTwo.cpp
+++ top20class/binaryTreeTwo.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
 #include<vector>
+#include<iterator>
+#include<unordered_map>
+
+
+/* Problem Statement:
+    Find the Left most Node from bottom of tree
+*/
 
 using namespace std;
 
@@ -14,6 +21,7 @@ class LeftMostNode {
 /* Using Queue:
     Doing Right to Left Push because Output required is Left Most Node
 
+    TC: O(n), SC: O(n)
 */
         int leftMostNode1(BTree *root) {
             BTree *tmp = NULL;
@@ -40,6 +48,11 @@ class LeftMostNode {
         // It is being Used just to Pick First Element when NULL Occurs ( To Pick First node from Bottom Level in Tree
         enum state { NULLSTATE, NON_NULLSTATE};
 
+/* Using Queue:
+    Doing Left to Right Push
+
+    TC: O(n), SC: O(n)
+*/
         int leftMostNode2(BTree *root) {
             int result= 0;
             BTree *tmp = NULL;
@@ -78,6 +91,41 @@ class LeftMostNode {
             }
             return result;
         }
+
+/*
+    Using Recursion:
+
+    Storing each Level Nodes into Hash Map and at last returning last Level's Left Most Node
+*/
+
+        void storeTree(unordered_map<int, vector <int> > &hMap, BTree *root, int level) {
+
+            if(root == NULL)
+                return;
+
+            storeTree(hMap, root->left, level+1);
+            hMap[level].push_back(root->data);
+            storeTree(hMap, root->right, level+1);
+        }
+
+        int leftMostNode3(BTree *root) {
+            int level = 1;
+            unordered_map<int, vector<int> > hMap;
+
+            storeTree(hMap, root, level);
+/*
+    To Print:
+            for(auto i = hMap.begin(); i != hMap.end(); ++i) {
+                cout << "Same Group of " << i->first << " : {";
+                for(auto j : i->second)
+                    cout << " " << j;
+                cout << "}" << endl;
+            }
+*/
+            auto i = hMap.size();
+
+            return hMap.at(i)[0];
+        }
 };
         
 int main() {
@@ -90,7 +138,8 @@ int main() {
     createOneChildTree();
 
     //cout << "Left Most Node is " << obj->leftMostNode1(bTreeUtil::root) << endl;
-    cout << "Left Most Node is " << obj->leftMostNode2(bTreeUtil::root) << endl;
+    //cout << "Left Most Node is " << obj->leftMostNode2(bTreeUtil::root) << endl;
+    cout << "Left Most Node is " << obj->leftMostNode3(bTreeUtil::root) << endl;
 
     return 0;
 }
