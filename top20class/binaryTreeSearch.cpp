c690fbf1be1cd5739acf988f481b174c8715eaff
Dheeraj <Dheeraj.embdeveloper@gmail.com>
2020-03-08 08:42:01 +0000

    Implemented Third Solution of Search Key Element in Tree Problem

--- top20class/binaryTreeSearch.cpp
+++ top20class/binaryTreeSearch.cpp
@@ -12,18 +12,18 @@ class BTreeSearch {
     public:
         int val; 
 
-/* Using Recursion:
+        /* Using Recursion:
 
-    TC: O(n) 
-    SC: O(n) -> n * stack Size
-*/
+            TC: O(n) 
+            SC: O(n) -> n * stack Size
+         */
         bool bTreeSearch1(BTree *root) {
 
             bool isTrue = false;
 
             if(root == NULL)
                 return false;
-            
+
             isTrue = bTreeSearch1(root->left);
             if(root->data == val)
                 return true;
@@ -36,12 +36,12 @@ class BTreeSearch {
             return isTrue;
         }
 
-/*
-    Using Queue:
-        After pop checking if Key element is matching node data or not 
-        TC: O(n)
-        SC: O(n)
-*/
+        /*
+            Using Queue:
+            After pop checking if Key element is matching node data or not 
+                TC: O(n)
+                SC: O(n)
+         */
         bool bTreeSearch2(BTree *root) {
 
             bool isTrue = false;
@@ -63,12 +63,69 @@ class BTreeSearch {
 
                 if(tmp->left != NULL)
                     queue.push_back(tmp->left);
-                
+
                 if(tmp->right != NULL)
                     queue.push_back(tmp->right);
             }
-                
+
             return false;
+        }
+
+        /*  
+            Find Successor
+            Using Null Checks
+                TC: O(n)
+                SC: O(1)
+         */
+        struct BTree *findSuccessor(BTree *root) {
+
+            BTree *tmp = root; 
+            BTree *prev = NULL;
+            tmp = tmp->right;
+
+            while(tmp!=NULL) {
+
+                if(tmp == root)
+                    break;
+
+                prev = tmp;
+                tmp = tmp->left;
+
+            }
+            return prev;
+        }
+
+        bool bTreeSearch3(BTree *root) {
+
+            bool isTrue = false;
+            BTree *tmp = root;
+            BTree *prev = NULL;
+
+            while(tmp != NULL) {
+
+                isTrue = (tmp->data == val) ? true : false;
+
+                if(isTrue)
+                    return true;
+
+                if(tmp->right != NULL) {
+                    prev = findSuccessor(tmp);
+
+                    if(prev->left != NULL) {
+                        cout << "For Check" << endl;
+                        prev->left = NULL;
+                        tmp = tmp->left;
+                    }
+                    else {
+                        prev->left = tmp;
+                        tmp = tmp->right;
+                    }
+                }
+                else if(tmp->left != NULL)
+                    tmp = tmp->left;
+
+            }
+            return false; 
         }
 };
 
@@ -84,7 +141,8 @@ int main() {
     createOneChildTree();
 
     //cout << "Element Existence : " << boolalpha << obj->bTreeSearch1(bTreeUtil::root) << endl;
-    cout << "Element Existence : " << boolalpha << obj->bTreeSearch2(bTreeUtil::root) << endl;
+    //cout << "Element Existence : " << boolalpha << obj->bTreeSearch2(bTreeUtil::root) << endl;
+    cout << "Element Existence : " << boolalpha << obj->bTreeSearch3(bTreeUtil::root) << endl;
 
 
     return 0;
